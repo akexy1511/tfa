2de2c4a5b9e818be332d2e44736146a504c47a88
akexy1511 <alexyviatour@hotmail.com>
2025-06-02 12:19:58 +0200

    Create delete_perso.php

---
+++ delete_perso.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supprimer un personnage</title>
+    <?php
+    include 'include/link.php';
+    ?>
+</head>
+<body>
+    <?php
+    include 'include/navbar.php';
+    ?>
+</body>
+</html>
